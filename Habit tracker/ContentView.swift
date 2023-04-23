ff5dc1c0bbf50f2da05e6ea5718ddae0e31ed449
youssefazroun <yhma22.azroyo@folkuniversitetet.nu>
2023-04-23 20:23:44 +0200

    Initial Commit

---
+++ Habit tracker/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Habit tracker
+//
+//  Created by Youssef Azroun on 2023-04-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
